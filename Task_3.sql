7f17465fadfb0fd8b51aa503ecf2e0c269563222
veneS-Sudo <58223048+veneS-Sudo@users.noreply.github.com>
2022-12-16 15:43:28 +0300

    Update and rename query.sql to Task_3.sql

--- Task_3.sql
+++ Task_3.sql
@@ -1,8 +1,15 @@
 -- Поскольку связь между продуктами и категориями "многие-ко-многим" то,
 -- между ними должна существовать промежуточная таблица, в хранении которой
 -- должны находится первичные ключы обоих отношений.
--- Предположим, что такая таблица с наименование product_category существует.
+-- Предположим, что такая таблица с наименование "product_description" существует.
 -- Тип первичного ключа (сурогатный или составной) в такой таблице для задачи неважен
-Select product.name, category.name
-FROM category INNER JOIN product_category ON category.id = product_category.category_id
-    RIGHT JOIN product ON product_category.product_id = product.id
+select 
+      products.name as Product
+    , categories.name as Category
+from products
+    left join
+     product_description 
+        on products.id = product_description.product_id
+    left join 
+     categories
+        on  = product_description.category_id = categories.id
